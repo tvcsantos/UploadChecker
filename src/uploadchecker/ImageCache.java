2f24324b6d6b5fbeed2915d349cc5b7024e42d72
Tiago Santos <tvcsantos@gmail.com>
2018-12-23 15:13:07 +0000

    reset repository

--- src/uploadchecker/ImageCache.java
+++
@@ -1,9 +0,0 @@
-package uploadchecker;
-
-import java.net.URL;
-
-/**
- *
- * @author camon
- */
-public class ImageCache extends GenericCache<String, URL> { }
--- src/uploadchecker/SearchCache.java
+++
@@ -1,9 +0,0 @@
-package uploadchecker;
-
-import java.util.List;
-
-/**
- *
- * @author camon
- */
-public class SearchCache extends GenericCache<String, List<SearchResult>> { }
